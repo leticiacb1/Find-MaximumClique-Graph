411327162349d2f9ab4e383a8218dc9105299c05
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-12-09 17:26:55 -0300

    Comment existing file line.

--- src/python/utils/clique_maximo_resposta.py
+++ src/python/utils/clique_maximo_resposta.py
@@ -6,9 +6,9 @@ def clique_maximo_resposta(nome_arquivo, debug = False):
         Utiliza uma biblioteca do python especializada para descobrir qual o clique máximo de um grafo.
     '''
 
-    if not os.path.exists(filename):
-        print(f"\n [ERROR] Arquivo {nome_arquivo} não existe! \n")  
-        return      
+    # if not os.path.exists(filename):
+    #     print(f"\n [ERROR] Arquivo {nome_arquivo} não existe! \n")  
+    #     return
 
     with open(nome_arquivo, 'r') as arquivo:
         next(arquivo)  
